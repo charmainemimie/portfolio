81c3c2c032ec274198f7d732c8543f15b6eb32b9
charmainemimie <82668352+charmainemimie@users.noreply.github.com>
2024-01-22 21:30:33 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Mimie from '../assets/mimie.jpg'
-import cv from '..assets/CV CharmaineSMangorima.pdf'
+import cv from '../assets/CV CharmaineSMangorima.pdf'
 const Hero = () => {
   return (
 
