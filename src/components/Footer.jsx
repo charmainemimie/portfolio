4d4f9c8617ec89b818be5e118395c2b91b6e03a0
charmiainemimie <charmainemangorima@gmail.com>
2024-03-26 11:36:16 +0200

    about

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import {FaFacebook,FaInstagram,FaLinkedinIn,FaEnvelope,FaTwitter,FaGithub} from "react-icons/fa"
+import {FaFacebook,FaInstagram,FaLinkedinIn,FaEnvelope,FaGithub} from "react-icons/fa"
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
     return (
@@ -9,7 +10,7 @@ const Footer = () => {
               <a title="LinkedIn" className="text-3xl  hover:text-blue-400" href="https://www.linkedin.com/in/charmaine-simangaliso-m-366770199/" target="_blank" rel="noreferrer"><FaLinkedinIn/></a>
               <a title="Facebook" className="text-3xl  hover:text-blue-400" href="https://web.facebook.com/charmaine.mangorima.3/" target="_blank" rel="noreferrer"><FaFacebook/></a>
               <a title="Instagram" className="text-3xl  hover:text-blue-400" href="https://www.instagram.com/charmaine_mimie/" target="_blank" rel="noreferrer"><FaInstagram/></a>
-              <a title="X / Twitter" className="text-3xl  hover:text-blue-400" href="https://twitter.com/2101msc?t=lEmnGZ-5daSVwJQu1hPN1w&s=09" target="_blank" rel="noreferrer"><FaTwitter/></a>
+              <a title="X" className="text-3xl  hover:text-blue-400" href="https://twitter.com/2101msc?t=lEmnGZ-5daSVwJQu1hPN1w&s=09" target="_blank" rel="noreferrer"><FaXTwitter/></a>
     </div>
   <div className="text-center mt-4">
   
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,119 +1,90 @@
-//import Header from "../components/NavigBar";
-import Footer from "../components/Footer";
+import { Progress } from "flowbite-react";
+import Footer from '../components/Footer'
+const About = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 px-8 pt-10 pb-8 gap-8">
+        <div className="">
+          <h1 className="pb-4 text-2xl font-bold">Skills</h1>
+          <div className="flex flex-col gap-2">
+            <div className="text-lg font-medium dark:text-white">HTML</div>
+            <Progress progress={99} size="xl" color="dark" />
+            <div className="text-lg font-medium dark:text-white">ReactJs</div>
+            <Progress progress={85} size="xl" color="dark" />
 
-import {
-  FaJava,
-  FaPython,
-  FaReact,
-  FaHtml5,
-  FaCss3,
-  FaBootstrap,
-  FaPhp,
-} from "react-icons/fa";
-import {
-  SiCplusplus,
-  SiJavascript,
-  SiTailwindcss,
-  SiOcaml,
-  SiMysql,
-} from "react-icons/si";
+            <div className="text-lg font-medium dark:text-white">Python</div>
+            <Progress progress={70} size="xl" color="dark" />
 
-const About = () => (
-  <div className="pt-12 border">
-    <h1 className="text-2xl decoration-blue-950 font-bold font-mono">
-      <marquee> About Charmaine Mangorima</marquee>
-    </h1>
-    <div className=" pt-4 lg:px-16">
-      <strong>
-        <u>
-          <p className="pb-3">Languages and Frameworks</p>
-        </u>
-      </strong>
+            <div className="text-lg font-medium dark:text-white">
+              JavaScript
+            </div>
+            <Progress progress={85} size="xl" color="dark" />
 
-      <div className="flex flex-wrap ">
-        <p className="flex gap-4  text-2xl">
-          <FaJava className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <SiCplusplus className="text-2xl" /> &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <FaPython className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <SiJavascript className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <FaReact className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <FaHtml5 className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4 pb-3 text-2xl">
-          &nbsp;
-          <FaCss3 className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <FaPhp className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <SiMysql className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <SiTailwindcss className="text-2xl" />  &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <SiOcaml className="text-2xl" /> &nbsp;
-        </p>
-        <p className="flex gap-4  text-2xl">
-          &nbsp;
-          <FaBootstrap className="text-2xl" /> &nbsp;
-        </p>
-      </div>
-      <br />
+            <div className="text-lg font-medium dark:text-white">Java</div>
+            <Progress progress={65} size="xl" color="dark" />
 
-      
-        <div className="mx-4 md:mx-8 lg:mx-16 xl:mx-32 pb-8">
-          Hello and welcome 😃 ! My name is Charmaine, and I am a Graduate
-          Software Engineer. I am passionate about technology and its ability to
-          make a positive impact on society. I have a Bachelor of Science degree
-          in Information Systems and Software Engineering from University of
-          Boumerdes ,Algeria and I graduated with a Master of Science
-          degree in Software Engineering and Information Processing from the
-          same university. I am excited to begin my career in the field.
-       
-      
-      <br /><br />
-      
-        
-          During my studies, I gained experience in various programming
-          languages such as Java, Python, and JavaScript, and I have worked on
-          projects ranging from simple console applications to full-stack web
-          development. I also have knowledge in network and computer security. I
-          am a quick learner, an analytical thinker, and a problem-solver. I
-          enjoy working collaboratively with others to solve complex problems,
-          and I am always looking for new challenges to expand my skill set. In
-          my free time, I enjoy tinkering with personal programming projects and
-          staying up-to-date with the latest industry trends. Thank you for
-          taking the time to learn more about me. Please feel free to explore my
-          portfolio to learn more about my experience and skills. If you have
-          any questions or would like to connect, please don&apos;t hesitate to
-          reach out!
+            <div className="text-lg font-medium dark:text-white">MySQL</div>
+            <Progress progress={85} size="xl" color="dark" />
+
+            <div className="text-lg font-medium dark:text-white">
+              TailwindCSS
+            </div>
+            <Progress progress={85} size="xl" color="dark" />
+            <div className="text-lg font-medium dark:text-white">C/C++</div>
+            <Progress progress={60} size="xl" color="dark" />
+
+            <div className="text-lg font-medium dark:text-white">Node</div>
+            <Progress progress={50} size="xl" color="dark" />
+
+            <div className="text-lg font-medium dark:text-white">
+              Express.js
+            </div>
+            <Progress progress={50} size="xl" color="dark" />
           </div>
-     
-    </div>
+</div>
+          <div>
+            <div className="about-me pt-4 pt-md-0">
+              <div className="title-box-2">
+              <h1 className="pb-4 text-2xl font-bold">About me</h1>
+              </div>
+              <p className="lead">
+                Hello and welcome 😃 !<br />
+                <br />
+                My name is Charmaine, and I am a Graduate Software Engineer. I am
+                passionate about technology and its ability to make a positive
+                impact on society. I have a Bachelor&apos;s degree in
+                Information Systems and Software Engineering from University of
+                Boumerdes ,Algeria and I am about to graduate with a
+                Master&apos;s degree in Software Engineering and Information
+                Processing from the same university.
+              </p>     <br />     <br />
+              <p className="lead">
+                I am excited to begin my career in the field. During my studies,
+                I gained experience in various programming languages such as
+                Java, Python, and JavaScript, and I have worked on projects
+                ranging from simple console applications to full-stack web
+                development. I also have knowledge in network and computer
+                security. I am a quick learner, an analytical thinker, and a
+                problem-solver. I enjoy working collaboratively with others to
+                solve complex problems, and I am always looking for new
+                challenges to expand my skill set. In my free time, I enjoy
+                tinkering with personal programming projects and staying
+                up-to-date with the latest industry trends.
+              </p> <br />     <br />
+              <p className="lead">
+                Thank you for taking the time to learn more about me. Please
+                feel free to explore my portfolio to learn more about my
+                experience and skills. If you have any questions or would like
+                to connect, please don&apos;t hesitate to reach out!
+              </p>
+            </div>
+          </div>
+        </div>
 
-    <Footer />
-  </div>
-);
+        <Footer/>
+      </div>
+
+  );
+};
 
 export default About;
